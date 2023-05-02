b222dc6567da679bd9521f4f7cdbdb0ce27b6fc4
Ashton Halat <78611739+ashtonhalat@users.noreply.github.com>
2023-05-02 13:20:14 -0500

    Add files via upload
    changed editCow and addCow

--- ios-app/AddCowViewController.swift
+++ ios-app/AddCowViewController.swift
@@ -9,21 +9,30 @@ import UIKit
 
 class AddCowViewController: UIViewController {
 
+    @IBOutlet weak var cowName: UITextField!
+    @IBOutlet weak var cowWeight: UITextField!
+    @IBOutlet weak var cowVaccinations: UITextField!
+    @IBOutlet weak var cowNotes: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func addCow(_ sender: Any) {
+        /*
+          (1)Are all data types of parameters to Record() correct?
+          (2)Add error handling to prevent user from entering bad input for numeric values such as Weight?
+          (3)Add additional text fields for birthYear, pregStatus, sex?
+            Or
+            Parse all information (birthYear, pregStatus, sex) from "Notes" text field?
+          (4)Is vaxStatus string or "Y/N"? Currently adds string that was input into "Vaccinations" text field. Could change to be "Y/N" depending on if box is emptpy
+         */
+        let con: DatabaseConnection = DatabaseConnection()
+        let record: Record = Record(cowId: cowName.text!, birthYear: "1920", vaxStatus: cowVaccinations.text!, lastWeight: Double(cowWeight.text!)!, pregStatus: 0, sex: "Intersex")
+        con.createRecord(Record: record)
+        print(con.dbUrl)
     }
-    */
-
 }
--- ios-app/EditCowViewController.swift
+++ ios-app/EditCowViewController.swift
@@ -9,21 +9,32 @@ import UIKit
 
 class EditCowViewController: UIViewController {
 
+    @IBOutlet weak var newCowName: UITextField!
+    @IBOutlet weak var newCowWeight: UITextField!
+    @IBOutlet weak var newCowVaccinations: UITextField!
+    @IBOutlet weak var newCowNotes: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func updateCow(_ sender: Any) {
+        /*
+          (1)Are all data types of parameters to Record() correct?
+          (2)Change updateEntryStmt to correctly access cow pointed to? Opaque Pointer
+          (3)Add error handling to prevent user from entering bad input for numeric values such as Weight? (Don't add cow if input is bad or give popup notification)
+          (4)Add additional text fields in EditCow screen and AddCow Screen for birthYear, pregStatus, sex?
+            Or
+            Parse all information (birthYear, pregStatus, sex) from "Notes" text field?
+          (5)Top displays Cow #32 (Name). Tapping on dot indicating cow (on map) will make this screen come up and label corresponding to cow will be displayed in future?
+         */
+        let con: DatabaseConnection = DatabaseConnection()
+        let record: Record = Record(cowId: newCowName.text!, birthYear: "1920", vaxStatus: newCowVaccinations.text!, lastWeight: Double(newCowWeight.text!)!, pregStatus: 0, sex: "Intersex")
+        con.updateEntryStmt(newCowName.text!)
+        
     }
-    */
-
 }
